33791efea157222162e6d35b7d4e5916264f0e03
Emma A <emmaalderson24@gmail.com>
2024-04-28 20:40:38 -0500

    tweaks to cat

--- lua/mh/plugins/catppuccin.lua
+++ lua/mh/plugins/catppuccin.lua
@@ -1,7 +1,7 @@
 return {
 	"catppuccin/nvim",
 	name = "catppuccin",
-	priority = 1000,
+	priority = 10000,
 	config = function()
 		require("catppuccin").setup({
 			flavour = "macchiato", -- latte, frappe, macchiato, mocha
